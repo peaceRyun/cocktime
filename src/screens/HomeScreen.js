def6abbe26835d59f93c717964ca90b4e88ef89a
peaceRyun <dbsrn1110@gmail.com>
2025-07-18 20:13:02 +0900

    홈화면 하단 마저 마크업 //

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -26,7 +26,7 @@ export default function HomeScreen({ navigation }) {
             </View>
             <View style={styles.prevMatchCont}>
                 <Text style={styles.prevMatchTitle}>Previous Matches</Text>
-                <View style={{ marginTop: 30 }}>
+                <View style={{ marginTop: 30, display: 'flex', gap: 30 }}>
                     <View style={styles.cardCont}>
                         <View style={{ display: 'flex', flexDirection: 'row', gap: 10 }}>
                             <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: 5 }}>
@@ -38,6 +38,195 @@ export default function HomeScreen({ navigation }) {
                                 <Text style={{ fontWeight: 600, fontSize: 15, color: '#5A86F1' }}>07:25</Text>
                             </View>
                         </View>
+                        <View>
+                            <View
+                                style={{
+                                    display: 'flex',
+                                    flexDirection: 'row',
+                                    alignItems: 'center',
+                                    justifyContent: 'space-between',
+                                    marginTop: 20,
+                                }}
+                            >
+                                <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: 5 }}>
+                                    <View style={styles.countryCont}></View>
+                                    <Text style={{ fontSize: 15, color: '#fff' }}>홍길동1</Text>
+                                    <Text style={{ fontSize: 15, color: '#fff' }}>홍길동2</Text>
+                                </View>
+                                <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: 15 }}>
+                                    <AntDesign name='checkcircle' size={15} color='#5A86F1' />
+                                    <Text style={{ fontSize: 15, color: '#fff' }}>25</Text>
+                                </View>
+                            </View>
+                            <Text style={{ fontSize: 14, color: '#5A86F1', marginVertical: 5 }}>VS</Text>
+                            <View
+                                style={{
+                                    display: 'flex',
+                                    flexDirection: 'row',
+                                    alignItems: 'center',
+                                    justifyContent: 'space-between',
+                                    opacity: 0.4,
+                                }}
+                            >
+                                <View
+                                    style={{
+                                        display: 'flex',
+                                        flexDirection: 'row',
+                                        alignItems: 'center',
+                                        gap: 5,
+                                    }}
+                                >
+                                    <View style={styles.countryCont}></View>
+                                    <Text style={{ fontSize: 15, color: '#fff' }}>홍길동1</Text>
+                                    <Text style={{ fontSize: 15, color: '#fff' }}>홍길동2</Text>
+                                </View>
+                                <View
+                                    style={{
+                                        display: 'flex',
+                                        flexDirection: 'row',
+                                        alignItems: 'center',
+                                        gap: 15,
+                                    }}
+                                >
+                                    {/* <AntDesign name='checkcircle' size={15} color='#5A86F1' /> */}
+                                    <Text style={{ fontSize: 15, color: '#fff' }}>20</Text>
+                                </View>
+                            </View>
+                        </View>
+                    </View>
+                    <View style={styles.cardCont}>
+                        <View style={{ display: 'flex', flexDirection: 'row', gap: 10 }}>
+                            <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: 5 }}>
+                                <AntDesign name='calendar' size={15} color='#5A86F1' />
+                                <Text style={{ fontWeight: 600, fontSize: 15, color: '#5A86F1' }}>2025/07/17</Text>
+                            </View>
+                            <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: 5 }}>
+                                <AntDesign name='clockcircleo' size={15} color='#5A86F1' />
+                                <Text style={{ fontWeight: 600, fontSize: 15, color: '#5A86F1' }}>07:25</Text>
+                            </View>
+                        </View>
+                        <View>
+                            <View
+                                style={{
+                                    display: 'flex',
+                                    flexDirection: 'row',
+                                    alignItems: 'center',
+                                    justifyContent: 'space-between',
+                                    marginTop: 20,
+                                }}
+                            >
+                                <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: 5 }}>
+                                    <View style={styles.countryCont}></View>
+                                    <Text style={{ fontSize: 15, color: '#fff' }}>홍길동1</Text>
+                                    <Text style={{ fontSize: 15, color: '#fff' }}>홍길동2</Text>
+                                </View>
+                                <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: 15 }}>
+                                    <AntDesign name='checkcircle' size={15} color='#5A86F1' />
+                                    <Text style={{ fontSize: 15, color: '#fff' }}>25</Text>
+                                </View>
+                            </View>
+                            <Text style={{ fontSize: 14, color: '#5A86F1', marginVertical: 5 }}>VS</Text>
+                            <View
+                                style={{
+                                    display: 'flex',
+                                    flexDirection: 'row',
+                                    alignItems: 'center',
+                                    justifyContent: 'space-between',
+                                    opacity: 0.4,
+                                }}
+                            >
+                                <View
+                                    style={{
+                                        display: 'flex',
+                                        flexDirection: 'row',
+                                        alignItems: 'center',
+                                        gap: 5,
+                                    }}
+                                >
+                                    <View style={styles.countryCont}></View>
+                                    <Text style={{ fontSize: 15, color: '#fff' }}>홍길동1</Text>
+                                    <Text style={{ fontSize: 15, color: '#fff' }}>홍길동2</Text>
+                                </View>
+                                <View
+                                    style={{
+                                        display: 'flex',
+                                        flexDirection: 'row',
+                                        alignItems: 'center',
+                                        gap: 15,
+                                    }}
+                                >
+                                    {/* <AntDesign name='checkcircle' size={15} color='#5A86F1' /> */}
+                                    <Text style={{ fontSize: 15, color: '#fff' }}>20</Text>
+                                </View>
+                            </View>
+                        </View>
+                    </View>
+                    <View style={styles.cardCont}>
+                        <View style={{ display: 'flex', flexDirection: 'row', gap: 10 }}>
+                            <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: 5 }}>
+                                <AntDesign name='calendar' size={15} color='#5A86F1' />
+                                <Text style={{ fontWeight: 600, fontSize: 15, color: '#5A86F1' }}>2025/07/17</Text>
+                            </View>
+                            <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: 5 }}>
+                                <AntDesign name='clockcircleo' size={15} color='#5A86F1' />
+                                <Text style={{ fontWeight: 600, fontSize: 15, color: '#5A86F1' }}>07:25</Text>
+                            </View>
+                        </View>
+                        <View>
+                            <View
+                                style={{
+                                    display: 'flex',
+                                    flexDirection: 'row',
+                                    alignItems: 'center',
+                                    justifyContent: 'space-between',
+                                    marginTop: 20,
+                                }}
+                            >
+                                <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: 5 }}>
+                                    <View style={styles.countryCont}></View>
+                                    <Text style={{ fontSize: 15, color: '#fff' }}>홍길동1</Text>
+                                    <Text style={{ fontSize: 15, color: '#fff' }}>홍길동2</Text>
+                                </View>
+                                <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: 15 }}>
+                                    <AntDesign name='checkcircle' size={15} color='#5A86F1' />
+                                    <Text style={{ fontSize: 15, color: '#fff' }}>25</Text>
+                                </View>
+                            </View>
+                            <Text style={{ fontSize: 14, color: '#5A86F1', marginVertical: 5 }}>VS</Text>
+                            <View
+                                style={{
+                                    display: 'flex',
+                                    flexDirection: 'row',
+                                    alignItems: 'center',
+                                    justifyContent: 'space-between',
+                                    opacity: 0.4,
+                                }}
+                            >
+                                <View
+                                    style={{
+                                        display: 'flex',
+                                        flexDirection: 'row',
+                                        alignItems: 'center',
+                                        gap: 5,
+                                    }}
+                                >
+                                    <View style={styles.countryCont}></View>
+                                    <Text style={{ fontSize: 15, color: '#fff' }}>홍길동1</Text>
+                                    <Text style={{ fontSize: 15, color: '#fff' }}>홍길동2</Text>
+                                </View>
+                                <View
+                                    style={{
+                                        display: 'flex',
+                                        flexDirection: 'row',
+                                        alignItems: 'center',
+                                        gap: 15,
+                                    }}
+                                >
+                                    {/* <AntDesign name='checkcircle' size={15} color='#5A86F1' /> */}
+                                    <Text style={{ fontSize: 15, color: '#fff' }}>20</Text>
+                                </View>
+                            </View>
+                        </View>
                     </View>
                 </View>
             </View>
@@ -46,7 +235,7 @@ export default function HomeScreen({ navigation }) {
 }
 
 const styles = StyleSheet.create({
-    container: { position: 'relative', flex: 1 },
+    container: { position: 'relative', flex: 1, backgroundColor: '#d9e2f8' },
     h2Cont: { marginTop: 40, marginHorizontal: 20 },
     matchCont: {
         margin: 20,
@@ -85,5 +274,12 @@ const styles = StyleSheet.create({
         color: 'black',
         textAlign: 'center',
         fontWeight: 600,
+    },
+    countryCont: {
+        width: 20,
+        height: 20,
+        borderRadius: 100,
+        backgroundColor: '#1D1D1D',
+        position: 'relative',
     },
 });
